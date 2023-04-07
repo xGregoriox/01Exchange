4ef7148596a86cdd38d5bebeaa2f52f0ad699e9e
G-r-e-g-R <grodrper@emeal.nttdata.com>
2023-04-07 11:43:11 -0500

    Jarvis - Seguridad JWT

--- exchange/src/main/java/com/bcp/pe/exchange/ExchangeApplication.java
+++ exchange/src/main/java/com/bcp/pe/exchange/ExchangeApplication.java
@@ -1,7 +1,16 @@
 package com.bcp.pe.exchange;
 
+import com.bcp.pe.exchange.infraestructure.security.JWTAuthorizationFilter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+@Slf4j
 
 @SpringBootApplication
 public class ExchangeApplication {
@@ -9,5 +18,18 @@ public class ExchangeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExchangeApplication.class, args);
 	}
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+		@Override
+
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+					.authorizeRequests()
+					.antMatchers(HttpMethod.POST, "/service/user").permitAll()
+					.anyRequest().authenticated();
+		}
+	}
 }
